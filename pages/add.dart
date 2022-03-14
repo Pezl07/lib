fa90ec6cc935b1fe3c2b29f2a90c8544d12cd00f
Thanatip Boonnat <62160012@go.buu.ac.th>
2022-03-14 19:31:50 +0700

    add service

--- pages/add.dart
+++ pages/add.dart
@@ -1,3 +1,4 @@
+import 'package:accordion/accordion.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
@@ -12,6 +13,8 @@ class AddPage extends StatefulWidget {
 class _AddPageState extends State<AddPage> {
   TextEditingController todo_title = TextEditingController();
   TextEditingController todo_detail = TextEditingController();
+  final _headerStyle = const TextStyle(
+      color: Color(0xffffffff), fontSize: 15, fontWeight: FontWeight.bold);
 
   @override
   Widget build(BuildContext context) {
@@ -24,208 +27,157 @@ class _AddPageState extends State<AddPage> {
           icon: Icon(Icons.arrow_back_ios),
         ),
         title: Center(
-          
           child: Text('SERVICE'),
         ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'SAVE',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        ],
         backgroundColor: Color.fromARGB(255, 1, 0, 73),
       ),
       body: ListView(
         children: [
-          Divider(
-            color: (Colors.black),
-          ),
-          Row(
-            children: [
-              Text(
-                'Import',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, color: Colors.black),
-              ),
-            ],
-          ),
-          Divider(
-            color: Colors.black,
-          ),
-          Row(
-            children: [
-              Text(
-                'Importer',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Importer',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            ],
-          ),
-          Divider(),
-          Row(
-            children: [
-              Text(
-                'Imported car',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Imported car',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            ],
-          ),
-          Divider(),
-          Row(
-            children: [
-              Text(
-                'Arrival data',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Arrival data',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            ],
-          ),
-          Divider(),
-          Row(
-            children: [
-              Text(
-                'Cut-off data',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Cut-off data',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            ],
-          ),
-          Divider(
-            color: Colors.black,
-          ),
-          Row(
-            children: [
-              Text(
-                'Export',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, color: Colors.black),
-              ),
-            ],
-          ),
-          Divider(
-            color: Colors.black,
-          ),
-          Row(
-            children: [
-              Text(
-                'Exporter',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Exporter',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            ],
-          ),
-          Divider(),
-          Row(
-            children: [
-              Text(
-                'Exported car',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Exported car',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            ],
-          ),
-          Divider(
-            color: Colors.black,
-          ),
-          Row(
-            children: [
-              Text(
-                'Location',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, color: Colors.black),
-              ),
-            ],
-          ),
-          Divider(
-            color: Colors.black,
-          ),
-          Row(
-            children: [
-              Text(
-                'Arrivals location',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Arrivals location',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            ],
-          ),
-          Divider(),
-          Row(
-            children: [
-              Text(
-                'Departure location',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Departure location',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-            ],
-          ),
-          Divider(),
+          //Import
+          Accordion(
+              maxOpenSections: 1,
+              headerBackgroundColorOpened: Colors.black54,
+              headerPadding:
+                  const EdgeInsets.symmetric(vertical: 17, horizontal: 17),
+              children: [
+                AccordionSection(
+                  isOpen: false,
+                  // leftIcon: const Icon(Icons.computer, color: Colors.white),
+                  header: Text('Import', style: _headerStyle ,),
+                  
+                  content: Column(
+                    children: [
+                      Divider(),
+                      Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: 'Import',
+                                border: OutlineInputBorder()),
+                            // textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          // ],
+                          // ),
+                          // Row(
+                          // children: [
+                          // Text('test'
+                          //   // decoration: new InputDecoration(
+                          //   //     border: InputBorder.none,
+                          //   //     focusedBorder: InputBorder.none,
+                          //   //     contentPadding: EdgeInsets.only(
+                          //   //         left: 15, bottom: 11, top: 11, right: 15),
+                          //   //     hintText: 'Subject'),
+                          //   // textAlign: TextAlign.center,
+                          //   // style: TextStyle(fontSize: 18, color: Colors.grey),
+                          // ),
+                        ],
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Imported car',
+                            border: OutlineInputBorder()),
+                        // textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Arrival data',
+                            border: OutlineInputBorder()),
+                        // textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Cut-off date',
+                            border: OutlineInputBorder()),
+                        // textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Divider(),
+                    ],
+                  ),
+                ),
+              ]),
+          //Export
+          Accordion(
+              maxOpenSections: 1,
+              headerBackgroundColorOpened: Colors.black54,
+              headerPadding:
+                  const EdgeInsets.symmetric(vertical: 17, horizontal: 17),
+              children: [
+                AccordionSection(
+                  isOpen: false,
+                  // leftIcon: const Icon(Icons.computer, color: Colors.white),
+                  header: Text('Export', style: _headerStyle),
+                  content: Column(
+                    children: [
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Exporter',
+                            border: OutlineInputBorder()),
+                        // textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Exported car',
+                            border: OutlineInputBorder()),
+                        // textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Divider(),
+                    ],
+                  ),
+                ),
+              ]),
+          //Location
+          Accordion(
+              maxOpenSections: 1,
+              headerBackgroundColorOpened: Colors.black54,
+              headerPadding:
+                  const EdgeInsets.symmetric(vertical: 17, horizontal: 17),
+              children: [
+                AccordionSection(
+                  isOpen: false,
+                  // leftIcon: const Icon(Icons.computer, color: Colors.white),
+                  header: Text('Location', style: _headerStyle),
+                  content: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Arrivals location',
+                            border: OutlineInputBorder()),
+                        // textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Departure location',
+                            border: OutlineInputBorder()),
+                        // textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Divider(),
+                    ],
+                  ),
+                ),
+              ]),
         ],
       ),
     );
